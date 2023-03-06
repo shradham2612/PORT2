b652084607b1be71fa8a469d0f5bd0c173d7b769
Shradha Mahajan <91412882+shradham2612@users.noreply.github.com>
2023-03-06 22:42:18 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Hero from "./hero";
+import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Projects from "./Projects";
 import Skills from "./Skills";
