bc9017938df16aa73fe27db8fab3acb0743eafad
shradham2612 <mahajanss_1@rknec.edu>
2023-03-29 00:17:25 +0530

    2nd commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,9 @@
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Hero from "./hero";
+import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import About from "./About";
 
-export {
-    Contact,
-    Footer,
-    Hero,
-    Navbar,
-    Projects,
-    Skills,
-    About,
-};
+export { Contact, Footer, Hero, Navbar, Projects, Skills, About };
