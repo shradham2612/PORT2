ad6add8eae9e55eedaa3d32adbe4d0a63eb86a7c
shradham2612 <mahajanss_1@rknec.edu>
2023-07-11 01:38:43 +0530

    pushing code

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,16 @@ import Projects from "./Projects";
 import Skills from "./Skills";
 import About from "./About";
 
+<<<<<<< HEAD
 export { Contact, Footer, Hero, Navbar, Projects, Skills, About };
+=======
+export {
+    Contact,
+    Footer,
+    Hero,
+    Navbar,
+    Projects,
+    Skills,
+    About,
+};
+>>>>>>> 7feb21be840ba379dcb2041466b131f31d4cd94f
